e3e903d04d220f1a4b8e5f248eda6bed76466cb8
Poveen <109457804+Poveen@users.noreply.github.com>
2025-08-08 10:43:09 +0530

    Add files via upload

---
+++ Difference of two numbers-A211.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    int n1,n2,dif;
+    printf("First Number : ");
+    scanf("%d", &n1);
+    printf("Second Number : ");
+    scanf("%d", &n2);
+    dif = n1 - n2;
+    printf("The difference is %d", dif);
+    return 0;
+}
