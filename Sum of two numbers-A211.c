b2782ffed3e691acd066bf2c626fba7a236ae879
Poveen <109457804+Poveen@users.noreply.github.com>
2025-08-08 10:53:34 +0530

    Update Sum of two numbers-A211.c
    void main ki jagah int main likhna tha. tabhi toh return 0 ka sense banta.

--- Sum of two numbers-A211.c
+++ Sum of two numbers-A211.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
-void main() {
+int main() {
     int n1,n2,sum;
     printf("First Number : ");
     scanf("%d", &n1);
@@ -10,3 +10,4 @@ void main() {
     printf("The sum is %d", sum);
     return 0;
 }
+
