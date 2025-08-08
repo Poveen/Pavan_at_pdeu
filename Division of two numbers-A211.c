283729d07786d0b0d3e57e50120d3a6ef0542bef
Poveen <109457804+Poveen@users.noreply.github.com>
2025-08-08 10:46:57 +0530

    Add files via upload

---
+++ Division of two numbers-A211.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    float n1,n2,div;
+    printf("First Number : ");
+    scanf("%f", &n1);
+    printf("Second Number : ");
+    scanf("%f", &n2);
+    div = n1 / n2;
+    printf("The division is %f", div);
+    return 0;
+}
