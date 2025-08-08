60179de4dbdac9e5b2dc1f6012d53490d9b462fc
Poveen <109457804+Poveen@users.noreply.github.com>
2025-08-08 10:39:54 +0530

    Add files via upload

---
+++ Sum of two numbers-A211.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    int n1,n2,sum;
+    printf("First Number : ");
+    scanf("%d", &n1);
+    printf("Second Number : ");
+    scanf("%d", &n2);
+    sum = n1 + n2;
+    printf("The sum is %d", sum);
+    return 0;
+}
