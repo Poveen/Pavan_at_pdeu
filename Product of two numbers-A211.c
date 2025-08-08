bd7fb8dfabefcd6ee0e7b7c0d25473af55ac431d
Poveen <109457804+Poveen@users.noreply.github.com>
2025-08-08 10:44:59 +0530

    Add files via upload

---
+++ Product of two numbers-A211.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    int n1,n2,prod;
+    printf("First Number : ");
+    scanf("%d", &n1);
+    printf("Second Number : ");
+    scanf("%d", &n2);
+    prod = n1 * n2;
+    printf("The product is %d", prod);
+    return 0;
+}
